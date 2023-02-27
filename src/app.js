e58ffe77c501be5544fec7f3c401802f93fd17f1
MesachVenegas <mesach.venegas@hotmail.com>
2023-02-27 01:45:23 -0800

    change: add db conection and test

--- src/app.js
+++ src/app.js
@@ -1,15 +1,26 @@
-const express = require('express');
 const morgan = require('morgan');
+const express = require('express');
+const toDos = require('./routes/todo.route');
+const TodoModel = require('./models/todos.route');
+const dbConnection = require('./utils/connection');
 
 
 const app = express();
 const PORT = 8000;
 
+
+dbConnection.authenticate()
+    .then(() => {
+        console.log('Connected to database');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
 app.use(morgan());
 app.use(express.json());
 
 
-
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });

ccd96ba15487e85d87a42f39f9b7139e0feab2aa
MesachVenegas <mesach.venegas@hotmail.com>
2023-02-27 00:50:26 -0800

    add: basic content of server

--- src/app.js
+++ src/app.js
@@ -1,2 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
+
+
+const app = express();
+const PORT = 8000;
+
+app.use(morgan());
+app.use(express.json());
+
+
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`);
+});
